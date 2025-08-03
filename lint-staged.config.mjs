cb4983a7936521414e8f1bb20bd983bcdd20c45f
GGORG <GGORG0@protonmail.com>
2025-08-03 18:13:15 +0200

    added prettier config

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,13 +1,13 @@
-import path from "path";
+import path from 'path';
 
 const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames
     .map((f) => path.relative(process.cwd(), f))
-    .join(" --file ")}`;
+    .join(' --file ')}`;
 
-const prettierCommand = "prettier --write --ignore-unknown";
+const prettierCommand = 'prettier --write --ignore-unknown';
 
-const jsGlob = "*.{js,jsx,ts,tsx}";
+const jsGlob = '*.{js,jsx,ts,tsx}';
 
 const config = {};
 

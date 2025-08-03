ca3d28826899ad14f099bd0fb1e0de68de7cef21
GGORG <GGORG0@protonmail.com>
2025-08-03 18:10:43 +0200

    added lint-staged

---
+++ lint-staged.config.mjs
@@ -0,0 +1,17 @@
+import path from "path";
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(" --file ")}`;
+
+const prettierCommand = "prettier --write --ignore-unknown";
+
+const jsGlob = "*.{js,jsx,ts,tsx}";
+
+const config = {};
+
+config[jsGlob] = [buildEslintCommand, prettierCommand];
+config[`!(${jsGlob})`] = prettierCommand;
+
+export default config;
